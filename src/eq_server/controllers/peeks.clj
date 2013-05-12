6cdf06d1d2b69a53e97658f32544044b23304e61
Thomas Steffes <smnirven@gmail.com>
2013-05-12 11:02:15 -0400

    adding some trace logging in peeks controller, fix nil peek limit bug

--- src/eq_server/controllers/peeks.clj
+++ src/eq_server/controllers/peeks.clj
@@ -4,6 +4,7 @@
             [eq-server.models.peek :as p]
             [eq-server.models.user :as u]
             [eq-server.models.egg  :as e]
+            [clojure.tools.logging :as log]
             [cheshire.core :refer :all]))
 
 ;; PARAMETER VALIDATIONS *************************************************************************************
@@ -13,9 +14,8 @@
   [params]
   (controller/validate-required-params! required-peek-params params)
   (let [user (u/find-user-by-guid (:user-guid params))
-        peek-limit (cond
-                     (:peek_limit user) (:peek_limit user)
-                    :else config/default-hourly-peek-limit)
+        peek-limit (if (:peek_limit user) (:peek_limit user)
+                     config/default-hourly-peek-limit)
         cnt (p/get-user-peek-count (:guid user))]
     (if (>= cnt peek-limit)
       (throw (ex-info "Slow down grasshopper" {:response-code 429})))))
@@ -23,11 +23,14 @@
 
 (defn create-peek
   [request]
-  (do (validate-params! (:params request))
+  (do (log/trace "Got a peek create request")
+    (log/trace (:params request))
+    (validate-params! (:params request))
     (let [params (:params request)
           resp {:status 200 :headers {"Content-Type" "application/json"}}
           user (u/find-user-by-guid (:user-guid params))
-          peek-distance (get user :peek_distance config/default-peek-distance)
+          peek-distance (if (:peek_distance user) (:peek_distance user)
+                          config/default-peek-distance)
           eggs (e/find-eggs-by-distance (:lat params)
                                         (:lng params)
                                         peek-distance
@@ -35,6 +38,9 @@
           egg-ids (map #(:id %) eggs)
           output-eggs (map #(dissoc % :point :id) eggs)]
       (do
+        (log/trace (str "peeking with lat: " (:lat params)
+                        " lng: " (:lng params)
+                        " peek-distance: " peek-distance))
         (p/create-peek! (assoc params :user-id (:id user)))
         (e/award-eggs! egg-ids (:id user)))
       (assoc resp :body (generate-string output-eggs)))))
