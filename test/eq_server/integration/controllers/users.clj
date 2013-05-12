a2f4d297d01b38bd34584d705d0d73916a7259ac
Thomas Steffes <smnirven@gmail.com>
2013-05-12 19:56:05 -0400

    added a bunch of user controller integration tests

--- test/eq_server/integration/controllers/users.clj
+++ test/eq_server/integration/controllers/users.clj
@@ -1,5 +1,6 @@
 (ns eq-server.integration.controllers.users
-  (:require [eq-server.routes :as r])
+  (:require [eq-server.routes :as r]
+            [eq-server.models.user :as u])
   (:use [eq-server.controllers.users]
         [midje.sweet]
         [ring.mock.request]))
@@ -39,3 +40,34 @@
                                                          :username "testy"}))]
          (:status resp) => 400
          (:headers resp) => (contains {"X-Error" "passwords don't match"})))
+
+(def user-guid (atom nil))
+
+(with-state-changes [(before :facts (reset! user-guid (u/create! {:email "testy@testy.com"
+                                                        :pwd "letmein"
+                                                        :pwd_conf "letmein"
+                                                        :username "testy"})))
+                     (after :facts (do (u/delete-user! @user-guid) (reset! user-guid nil)))]
+  (facts "authenticate happy path works properly"
+    (let [resp (r/app (request :post "/users/authenticate" {:email "testy@testy.com"
+                                                           :pwd "letmein"}))]
+      (:status resp) => 200))
+
+  (facts "authenticate sad path works properly"
+    (let [resp (r/app (request :post "/users/authenticate" {:email "testy@testy.com"
+                                                            :pwd "icanhazpwd?"}))]
+      (:status resp) => 401))
+
+  (facts "authenticate unknown user works properly"
+    (let [resp (r/app (request :post "/users/authenticate" {:email "idontexist@testy.com"
+                                                            :pwd "dontmatter"}))]
+      (:status resp) => 401
+      (:headers resp) => (contains {"X-Error" "User idontexist@testy.com does not exist"})))
+
+  (facts "creating a user that already exists shits the bed"
+    (let [resp (r/app (request :post "/users/create" {:email "testy@testy.com"
+                                                      :pwd "letmein"
+                                                      :pwd_conf "letmein"
+                                                      :username "testy"}))]
+      (:status resp) => 400
+      (:headers resp) => (contains {"X-Error" "User testy@testy.com already exists"}))))
