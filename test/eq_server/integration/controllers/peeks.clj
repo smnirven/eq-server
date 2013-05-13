285066026c58203e5a1038ea532ba4d2e7956ed8
Thomas Steffes <smnirven@gmail.com>
2013-05-13 06:08:24 -0400

    added a test for hourly peek limit

--- test/eq_server/integration/controllers/peeks.clj
+++ test/eq_server/integration/controllers/peeks.clj
@@ -1,5 +1,7 @@
 (ns eq-server.integration.controllers.peeks
-  (:require [eq-server.routes :as r])
+  (:require [eq-server.routes :as r]
+            [eq-server.models.user :as u]
+            [eq-server.config :as config])
   (:use [eq-server.controllers.peeks]
         [midje.sweet]
         [ring.mock.request]))
@@ -14,7 +16,35 @@
          (:status resp) => 400
          (:headers resp) => (contains {"X-Error" "lng is a required parameter"})))
 
-(facts "user_guid is enforced as a required parameter"
+(facts "user-guid is enforced as a required parameter"
        (let [resp (r/app (request :post "/peeks/create" {:lat 45.678 :lng 45.678}))]
          (:status resp) => 400
          (:headers resp) => (contains {"X-Error" "user-guid is a required parameter"})))
+
+(defn- do-a-bunch-of-peeks!
+  [guid lat lng num-requests]
+  (if (zero? num-requests)
+    nil
+    (do
+      (r/app (request :post "/peeks/create" {:user-guid guid :lat lat :lng lng}))
+      (recur guid lat lng (dec num-requests)))))
+
+
+(def user-guid (atom nil))
+
+(with-state-changes [(before :facts (reset! user-guid (u/create! {:email "testy@testy.com"
+                                                                  :pwd "letmein"
+                                                                  :pwd_conf "letmein"
+                                                                  :username "testy"})))
+                     (after :facts (do (u/delete-user! @user-guid) (reset! user-guid nil)))]
+
+  (facts "user hitting the peek limit is handled properly"
+    (do
+      ;; First use up the hourly peek limit for a user
+      (do-a-bunch-of-peeks! @user-guid 34.567 34.567 config/default-hourly-peek-limit)
+      ;; Than do one more, make sure the peek limit is enforced
+      (let [resp (r/app (request :post "/peeks/create" {:user-guid @user-guid
+                                                        :lat 34.567
+                                                        :lng 34.567}))]
+        (:status resp) => 429
+        (:headers resp) => (contains {"X-Error" "Slow down grasshopper"})))))
