9cda214d3af29c4fa6beb501450324d0afef1e9f
Thomas Steffes <smnirven@gmail.com>
2013-05-26 22:23:37 -0400

    adding new beanstalk env

--- project.clj
+++ project.clj
@@ -23,4 +23,5 @@
                  [com.netflix.curator/curator-framework "1.3.3"]
                  [com.netflix.curator/curator-recipes "1.3.3"]]
   :ring {:handler eq-server.routes/app :init eq-server.core/init! :auto-reload? true}
-  :aws {:beanstalk {:environments [{:name "eq-dev" :cname-prefix "eq-dev"}]}})
+  :aws {:beanstalk {:environments [{:name "eq-prod" :cname-prefix "eq-prod"}
+                                   {:name "eq-dev" :cname-prefix "eq-dev"}]}})
